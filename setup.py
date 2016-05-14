d17b2b221021562da89174b63b1a2bf98850d9d0
lonetwin <steve@lonetwin.net>
2016-05-14 19:31:14 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name = 'expyre',
-    version = '0.1.0',
+    version = '0.1.1',
     description = 'Python wrapper over `atd` to schedule deletion of files.',
     long_description = long_description,
     url = 'https://github.com/lonetwin/expyre',
